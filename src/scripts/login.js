eb873905e7aa1acaaf75197dddc13ab6b4dc2aee
Matheus Imazu Yoshida <yoshidaimatheus@gmail.com>
2025-10-05 19:43:01 -0300

    feat: add function to log in

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,3 +1,24 @@
-document.querySelector('.create-account-button').addEventListener("click", () => {
-    window.location.href = 'cadastro.html'
+document.querySelector('.create-account-button').addEventListener('click', () => {
+    window.location.href = 'cadastro.html';
+});
+
+document.querySelector('#loginButton').addEventListener('click', () => {
+    const emailValue = document.querySelector('#emailInput').value;
+    const passwordValue = document.querySelector('#passwordInput').value;
+    const listProfiles = JSON.parse(localStorage.getItem('listProfiles')) || null;
+    let credentialsIsValid = false;
+    
+    if(listProfiles){
+        listProfiles.forEach(profile => {
+            if(emailValue === profile.email && passwordValue === profile.password){
+                window.location.href = 'principal.html';
+                credentialsIsValid = true;
+                return;
+            } 
+        });
+    }
+
+    if(!credentialsIsValid){
+        alert('Incorrect email or password!');
+    }
 })

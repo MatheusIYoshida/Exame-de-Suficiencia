29873d5abf6bda3a5661c2fda9045697768f8126
Matheus Imazu Yoshida <yoshidaimatheus@gmail.com>
2025-10-08 23:50:45 -0300

    feat : funtions to remove user and search user - add new column

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -2,9 +2,33 @@ document.addEventListener('DOMContentLoaded', () => {
     tableList();
 });
 
+document.querySelector('#searchList').addEventListener('keyup', () => {
+    const search = document.querySelector('#searchList').value.toLowerCase();
+    const tableRows = document.querySelectorAll('table tr');
+
+    tableRows.forEach((row, index) => {
+        if(index == 0){
+            return;
+        }
+
+        let found = false;
+        const cells = row.querySelectorAll("td");
+
+        cells.forEach(cell => {
+            const text = cell.textContent.toLowerCase();
+            if(text.includes(search)){
+                found = true;
+            }
+        });
+
+        row.style.display = found ? "" : "none";
+    })
+});
+
 document.querySelector('#createUser').addEventListener('click', () => {
     let name = document.querySelector('#nameInput').value;
     let email = document.querySelector('#emailInput').value;
+    const date = new Date().toLocaleDateString('pt-BR');
     const listUsers = JSON.parse(localStorage.getItem('listUsers')) || [];
 
     const emailExists = listUsers.some(user => user.email === email);
@@ -16,7 +40,8 @@ document.querySelector('#createUser').addEventListener('click', () => {
 
     const newUser = {
         name: name,
-        email: email
+        email: email,
+        date: date
     }
 
     listUsers.push(newUser);
@@ -29,6 +54,11 @@ document.querySelector('#createUser').addEventListener('click', () => {
 document.querySelector('#clearInput').addEventListener('click', () => {
     document.querySelector('#nameInput').value = "";
     document.querySelector('#emailInput').value = "";
+});
+
+document.querySelector('#clearList').addEventListener('click', () => {
+    localStorage.removeItem('listUsers');
+    tableList();
 });
 
 function tableList(){
@@ -48,14 +78,27 @@ function tableList(){
             cellEmail.textContent = user.email;
             row.appendChild(cellEmail);
 
+            const cellDate = document.createElement('td');
+            cellDate.textContent = user.date;
+            row.appendChild(cellDate);
+
             const cellRemove = document.createElement('td');
             const removeIcon = document.createElement('img');
             removeIcon.src = 'src/images/trash.png';
             removeIcon.className = 'remove-icon';
+            removeIcon.addEventListener('click', () => removeUser(user.email));
             cellRemove.appendChild(removeIcon);
             row.appendChild(cellRemove);
 
             table.appendChild(row);
         }); 
     }
+}
+
+function removeUser(email){
+    const listUsers = JSON.parse(localStorage.getItem('listUsers'));
+    const updatedList = listUsers.filter(user => user.email !== email);
+    localStorage.removeItem('listUser');
+    localStorage.setItem('listUsers', JSON.stringify(updatedList));
+    tableList();
 }
